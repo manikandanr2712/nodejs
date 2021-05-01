47120fe3ec7067fec07dd7413e54f2a437259bf1
MANIKANDAN R <rmanikandan901@gmail.com>
2021-05-01 18:07:36 +0530

    master commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 // create express app
 const app = express();
 // Setup server port
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
@@ -17,6 +17,6 @@ const employeeRoutes = require('./src/routes/employee.routes')
 // using as middleware
 app.use('/api/v1/employees', employeeRoutes)
 // listen for requests
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+app.listen(process.env.PORT || 5000, function() {
+  console.log(`Server is listening on port `);
 });
